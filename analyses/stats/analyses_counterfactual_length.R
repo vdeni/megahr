f23382078cbbe88e31d9d3976dcfec38809f5c04
Denis Vlašiček <denis.vlasicek@gmail.com>
2023-11-19 18:46:13 +0100

    fixed matrix indexing issue

--- analyses/stats/analyses_counterfactual_length.R
+++ analyses/stats/analyses_counterfactual_length.R
@@ -24,10 +24,11 @@ names(posterior_means) <- model_summary$variable
 
 # select random sample of draws
 model_draws <- model_fit$draws()
+N_draws <- 125
 set.seed(1)
 idx_sample <- sample(
   x = 1:dim(model_draws)[1],
-  size = 125,
+  size = N_draws,
   replace = FALSE
 )
 model_draws_sample <- model_draws[idx_sample, , ]
@@ -48,19 +49,24 @@ counterfactual_model <- cmdstanr::cmdstan_model(
   stanc_options = list("O1")
 )
 
-model_draws_sample[, , "alpha_0"]
-
-l_data[["N_subsample"]] <- 500
-l_data[["max_wordlen"]] <- 5
-# l_data[["beta_subfreq"]] <- model_draws_sample[[, , "beta_subfreq"]]
-# l_data[["beta_aoa"]] <- model_draws_sample[[, , "beta_aoa"]]
-# l_data[["beta_concreteness"]] <- model_draws_sample[[, , "beta_concreteness"]]
+l_data[["N_subsample"]] <- N_draws
+l_data[["min_wordlen"]] <- 2
+l_data[["max_wordlen"]] <- 12
 l_data[["alpha_0_fixed"]] <- posterior_means[["alpha_0"]]
 l_data[["sigma_rt_fixed"]] <- posterior_means[["sigma_rt"]]
+l_data[["sub_freq_fixed"]] <- 3
+l_data[["aoa_fixed"]] <- 18
+l_data[["concreteness_fixed"]] <- 3
 
-counterfactual_model$generate_quantities(
+counterfactual_lengths <- counterfactual_model$generate_quantities(
   model_draws_sample,
   data = l_data,
   parallel_chains = 4,
   sig_figs = 4
+)
+
+counterfactual_lengths_summary <- counterfactual_lengths$summary(
+  "mean" = mean,
+  "median" = median,
+  "quantiles" = ~ quantile(., probs = c(.025, .975))
 )
--- analyses/stats/analyses_counterfactual_length.stan
+++ analyses/stats/analyses_counterfactual_length.stan
@@ -3,16 +3,13 @@ functions {
 }
 data {
   #include analyses_model_blk_data.stan
-
   int N_subsample;
 
+  int<lower=1> min_wordlen;
   int<lower=1> max_wordlen;
 
   real alpha_0_fixed;
   real<lower=0> sigma_rt_fixed;
-  // real beta_subfreq;
-  // real beta_aoa;
-  // real beta_concreteness;
 
   real sub_freq_fixed;
   real aoa_fixed;
@@ -22,10 +19,10 @@ parameters {
   #include analyses_model_blk_parameters.stan
 }
 generated quantities {
-  matrix[N_subsample, max_wordlen] rt_rep;
+  matrix[N_subsample, max_wordlen - min_wordlen + 1] rt_rep;
 
   for (i in 1:N_subsample) {
-    for (j in 2:max_wordlen) {
+    for (j in min_wordlen:max_wordlen) {
       real mu = alpha_0_fixed +
         beta_length * j +
         beta_subfreq * sub_freq_fixed +

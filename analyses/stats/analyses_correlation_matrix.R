68e9154bb67f0f524d4de5358f388e84fe58e9c4
Denis Vlašiček <denis.vlasicek@gmail.com>
2024-11-27 16:43:36 +0100

    added helper functions for paris plotting

--- analyses/stats/analyses_correlation_matrix.R
+++ analyses/stats/analyses_correlation_matrix.R
@@ -5,10 +5,29 @@ library(dplyr)
 library(ggplot2)
 library(stringr)
 
+ggplot2::theme_set(ggplot2::theme_minimal())
+ggplot2::theme_update(
+    panel.grid.major.x = ggplot2::element_line(
+        color = "gray",
+        size = 0.7
+    ),
+    panel.grid.major.y = ggplot2::element_line(
+        color = "gray",
+        size = 0.7
+    )
+)
+
 source(
     here::here(
         "helpers",
-        "pooled_statistics.R"
+        "pooled-statistics.R"
+    )
+)
+
+source(
+    here::here(
+        "helpers",
+        "plotting.R"
     )
 )
 
@@ -67,4 +86,29 @@ d_combined <- dplyr::left_join(
     y = d_psyling_deduped,
     suffix = c("_megart", "_psyling"),
     by = "string"
+)
+
+.hist_plot(
+    d_combined,
+    ggplot2::aes(x = stimulus_rt),
+    axis_name = "Reaction time (ms)",
+    bins = 500,
+    x_limits = c(0, 3000)
+)
+
+.scatter_plot(
+    d_combined,
+    ggplot2::aes(
+        x = stimulus_rt,
+        y = aoa
+    ),
+    axis_label_x = "Reaction time (ms)",
+    axis_label_y = "Age of acquisition",
+    x_limits = c(0, 3000),
+    y_limits = c(0, 16)
+)
+
+.corr_plot(
+    d_combined$stimulus_rt,
+    d_combined$subjective_frequency
 )
---
+++ helpers/plotting.R
@@ -0,0 +1,86 @@
+library(ggplot2)
+library(glue)
+
+.hist_plot <- function(
+    data,
+    mapping,
+    axis_name,
+    bins = 30,
+    x_limits = NULL) {
+    p <- ggplot2::ggplot(
+        data = data,
+        mapping = mapping
+    ) +
+        ggplot2::geom_histogram(
+            fill = "black",
+            bins = bins,
+            linewidth = 0
+        ) +
+        ggplot2::scale_x_continuous(
+            name = axis_name,
+        ) +
+        ggplot2::scale_y_continuous(
+            name = "Count"
+        ) +
+        ggplot2::coord_cartesian(
+            xlim = x_limits
+        )
+
+    return(p)
+}
+
+.scatter_plot <- function(
+    data,
+    mapping,
+    axis_label_x,
+    axis_label_y,
+    x_limits = NULL,
+    y_limits = NULL) {
+    p <- ggplot2::ggplot(
+        data = data,
+        mapping = mapping
+    ) +
+        ggplot2::geom_point(
+            size = 2,
+            alpha = 0.3,
+            color = "black",
+            fill = "black",
+            shape = 21,
+            stroke = 0
+        ) +
+        ggplot2::scale_x_continuous(name = axis_label_x) +
+        ggplot2::scale_y_continuous(name = axis_label_y) +
+        ggplot2::coord_cartesian(
+            xlim = x_limits,
+            ylim = y_limits
+        )
+
+    return(p)
+}
+
+.corr_plot <- function(
+    var_x,
+    var_y) {
+    res <- cor.test(x = var_x, y = var_y, method = "pearson")
+
+    if (round(res$p.value, 3) == 0) {
+        p_val <- "p < 0.01"
+    } else {
+        p_val <- glue::glue("p = {round(res$p.value, 3)}")
+    }
+
+    ggplot2::ggplot() +
+        ggplot2::geom_text(
+            data = data.frame(
+                x = glue::glue(
+                    "r = {round(res$estimate, 3)} ({round(res$conf.int[1], 3)}, {round(res$conf.int[2], 3)})",
+                    "t({res$parameter}) = {round(res$statistic, 3)}",
+                    "{p_val}",
+                    .sep = "\n"
+                )
+            ),
+            mapping = ggplot2::aes(label = x, x = 5, y = 5),
+            size = 20
+        ) +
+        ggplot2::theme_void()
+}
